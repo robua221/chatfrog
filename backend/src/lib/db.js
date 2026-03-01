7cb46c9984c6c441fb740d929e2b3aa1a630e237
ROBIN <robinalexander20@gmail.com>
2026-03-01 07:33:56 -0500

    email setup

---
+++ backend/src/lib/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import { ENV } from "./env.js";
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(ENV.MONGO_URI);
+    console.log("MONGODB CONNECTED", conn.connection.host);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,24 +1,28 @@
 import express from "express";
-import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import path from "path"
-dotenv.config();
-
-const PORT = process.env.PORT || 3000;
+import path from "path";
+import { connectDB } from "./lib/db.js";
+import { ENV } from "./lib/env.js";
 
 const app = express();
-const __dirname=path.resolve()
+const __dirname = path.resolve();
+const PORT = ENV.PORT || 3000;
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
-app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
-})
-
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-app.listen(3000, () => console.log(`connected to port ${PORT}`));
+app.listen(3000, () => {
+  console.log(`connected to port ${PORT}`);
+
+  connectDB();
+});
